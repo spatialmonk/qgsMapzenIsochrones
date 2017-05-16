7105ccd09dba2f998897a5fcad37b1a6569f7840
spatialmonk <ethangmonk@gmail.com>
2017-05-16 11:03:02 -0500

    Update isochrone_gen.py

--- isochrone_gen.py
+++ isochrone_gen.py
@@ -9,11 +9,21 @@ Created by: Ethan Monk
 
 from PyQt4.QtCore import QSettings, QTranslator, qVersion, QCoreApplication
 from PyQt4.QtGui import QAction, QIcon
+
+from qgis.core import *
+import qgis.utils
+
+from PyQt4.QtCore import QSettings, QTranslator, qVersion, QCoreApplication
+from PyQt4.QtGui import QAction, QIcon
 # Initialize Qt resources from file resources.py
 import resources
 # Import the code for the dialog
 from mapzen_isochrones_dialog import MapzenIsochronesDialog
 import os.path
+import urllib2
+from PyQt4.QtGui import QMessageBox
+import json
+import requests
 
 
 
@@ -34,10 +44,46 @@ class isochrone_gen:
 	self.dlg.comboBox.addItem('multimodal')
 	
 	
-	# Populate comboBox_2 with all point file layers
-	#for layer in qgis.utils.iface.legendInterface().layers():
-    #        layerType = layer.type()
-    #        if layerType == QgsMapLayer.VectorLayer and layer.wkbType() == QGis.WKBPoint:
-    #            self.dlg.comboBox_2.addItem(layer.name())
-    #              
-    
+	#Populate comboBox_2 with all point file layers
+	for layer in qgis.utils.iface.legendInterface().layers():
+            layerType = layer.type()
+            if layerType == QgsMapLayer.VectorLayer and layer.wkbType() == QGis.WKBPoint:
+                self.dlg.comboBox_2.addItem(layer.name())
+                  
+    #SET API Parameters
+	self.url = 'http://matrix.mapzen.com/isochrone?json='
+	self.api_key = self.dlg.lineEdit.text()
+	self.Latitude = self.dlg.lineEdit_2.text()
+	self.Longitude = self.dlg.lineEdit_3.text()
+	self.Minutes = self.dlg.spinBox.value()
+	self.CostModel = self.dlg.comboBox.currentText
+	
+	#Define API GET String
+	#getString = #'{"locations":[{"lat":{}{},"lon":{}}],"costing":"{}","contours":[{"time":{},"color":"ff0000"}]}&id=MapzenIsochr#one&api_key={}'.format(self.url, self.Latitude, self.Longitude, self.CostModel, self.Minutes, self.api_key)
+	
+	
+	getString = """http://matrix.mapzen.com/isochrone?json={"locations":[{"lat":32.734176,"lon":-97.333325}],"costing":"pedestrian","contours":[{"time":15,"color":"ff0000"}]}&id=Walk_From_Office&api_key=mapzen-bhbvdDT"""
+	
+	
+	
+	response = requests.get(getString)
+	root = json.loads(response.text)
+	
+	
+	vlayer = QgsVectorLayer("Polygon?crs=EPSG:4326","root","memory")
+	QgsMapLayerRegistry.instance().addMapLayer(vlayer)
+
+	
+
+	
+	#QgsMessageLog.logMessage("response")
+	QMessageBox.information(None, "DEBUG:", str(root)) 
+	
+	#print root
+	
+	#self.Fname = “John”
+	#self.Lname = “Doe”
+	#selfAge = “24”
+	#print “{} {} is {} years old.“ format(fname, lname, age)
+	
+	
